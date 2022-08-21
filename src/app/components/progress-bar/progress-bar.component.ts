b555b2ee699a0508ef10ae42cacaba0b53324f72
Jeniffer Callou <jenifferprc@gmail.com>
2022-08-21 21:24:32 +0200

    add var to ts component

--- src/app/components/progress-bar/progress-bar.component.ts
+++ src/app/components/progress-bar/progress-bar.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.scss']
 })
-export class ProgressBarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProgressBarComponent {
+  @Input() public value = 0;
+  @Input() public max = 100;
 }
