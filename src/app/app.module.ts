d2c99b8a316162d193b26f3deab6d6c5da148f60
Jeniffer Callou <jenifferprc@gmail.com>
2022-08-23 21:26:59 +0200

    add top-of-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CardComponent } from './components/card/card.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { StarRatingsComponent } from './components/star-ratings/star-ratings.component';
+import { TopOfPageComponent } from './components/top-of-page/top-of-page.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { StarRatingsComponent } from './components/star-ratings/star-ratings.com
     AccordionComponent,
     ProgressBarComponent,
     StarRatingsComponent,
-  ],
+    TopOfPageComponent,
+],
   imports: [
     BrowserModule,
     AppRoutingModule
