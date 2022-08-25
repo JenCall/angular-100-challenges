04e1233172550771ff17eebbd0d495eb88e7985a
Jeniffer Callou <jenifferprc@gmail.com>
2022-08-25 22:18:13 +0200

    remove piece of code from here to comp.documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AccordionItem } from './components/accordion/accordion-item.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,20 +6,5 @@ import { AccordionItem } from './components/accordion/accordion-item.interface';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-100-challenges';
-
-  public accordionItems: AccordionItem[] = [
-    {
-      title: 'Example 1',
-      content: 'Example Content 1',
-      isExpanded: false,
-    },
-    {
-      title: 'Example 2',
-      content: 'Example Content 2',
-      isExpanded: false,
-    }
-  ];
-
-  public progressValue = 25;
+  
 }
