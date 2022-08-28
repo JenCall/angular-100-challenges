474a404470e74356161af47b3c77472de390ea63
Jeniffer Callou <jenifferprc@gmail.com>
2022-08-28 21:24:13 +0200

    add truncate pipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PipeDocumentationComponent } from './pipe-documentation/pipe-documentation.component';
 import { RouterModule } from '@angular/router';
 import { PIPES_ROUTES } from './pipes.routes';
+import { TruncateComponent } from './truncate/truncate/truncate.component';
 
 
 @NgModule({
-  declarations: [PipeDocumentationComponent],
+  declarations: [PipeDocumentationComponent, TruncateComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(PIPES_ROUTES)
