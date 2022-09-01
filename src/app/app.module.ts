0c118c99df0f8a76dd5b5032f6ff95d6c35d526b
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-01 05:25:37 +0200

    update app module with correct imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { CardComponent } from './components/card/card.component';
-import { AccordionComponent } from './components/accordion/accordion.component';
-import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
-import { StarRatingsComponent } from './components/star-ratings/star-ratings.component';
 import { TopOfPageComponent } from './components/top-of-page/top-of-page.component';
 import { RouterModule } from '@angular/router';
-import { ComponentDocumentationComponent } from './components/component-documentation/component-documentation.component';
 import { APP_ROUTES } from './app.routes';
-import { PipeDocumentationComponent } from './pipes/pipe-documentation/pipe-documentation.component';
-import { ServiceDocumentationComponent } from './services/service-documentation/service-documentation.component';
-import { LoaderComponent } from './components/loader/loader.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
-    CardComponent,
-    AccordionComponent,
-    ProgressBarComponent,
-    StarRatingsComponent,
     TopOfPageComponent,
-    ComponentDocumentationComponent,
-    ServiceDocumentationComponent,
-    LoaderComponent,
-],
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
     RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [],
