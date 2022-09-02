5cdf7aeaabc7d0e63dff1cbb4027090ef9ac970a
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-02 19:25:41 +0200

    add directives module content

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
+import { DIRECTIVES_ROUTES } from './directives.routes';
+import { DirectiveDocumentationComponent } from './directive-documentation/directive-documentation.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DirectiveDocumentationComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(DIRECTIVES_ROUTES)
+  ],
+  providers: [
+    { provide: 'Navigator', useValue: navigator },
+    { provide: 'Document', useValue: document },
+    { provide: 'Window', useValue: window },
   ]
 })
 export class DirectivesModule { }
