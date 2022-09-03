8f36da6053c79da605d7046b9e2e965163b641ad
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-03 19:51:33 +0200

    delete template as need adjustments and credit card ts modified

--- src/app/pipes/credit-card-formatter/credit-card-formatter.pipe.ts
+++ src/app/pipes/credit-card-formatter/credit-card-formatter.pipe.ts
@@ -7,8 +7,21 @@ export class CreditCardFormatterPipe implements PipeTransform {
 
   public transform(cardNumber: string): string {
     if (!this.hasCorrectLength(cardNumber)) {
-      return 'Invalid Card Length';
+      return 'Invalid Card Length.';
     }
+
+    if (!this.isAllNumbers(cardNumber)) {
+      return 'Invalid Characters.';
+    }
+
+    return this.formatCardNumber(cardNumber);
+  }
+
+  private isAllNumbers(cardNumber: string): boolean {
+    const stringNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const totalValidCharacters = cardNumber.split('').filter((char) => stringNumbers.includes(char)).length;
+
+    return totalValidCharacters === cardNumber.length;
   }
 
   private hasCorrectLength(cardNumber: string): boolean {
@@ -19,6 +32,13 @@ export class CreditCardFormatterPipe implements PipeTransform {
     if (cardNumberLength === isAmericanExpressCardLength || cardNumberLength === isMasterDiscoverVisaCardLength) {
       return true;
     }
+
     return false;
+  }
+
+  private formatCardNumber(cardNumber: string): string {
+    const parts = cardNumber.match(/[\s\S]{1,4}/g);
+
+  return cardNumber;
   }
 }
