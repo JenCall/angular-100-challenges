ed4e7887fd870d407a91eb2758ec6f1b5104fb93
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-03 19:36:51 +0200

    add service module

---
+++ src/app/services/services.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ServiceDocumentationComponent } from './service-documentation/service-documentation.component';
+import { SERVICES_ROUTES } from './services.routes';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [ServiceDocumentationComponent],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    SharedModule,
+    RouterModule.forChild(SERVICES_ROUTES)
+  ]
+})
+export class ServicesModule { }
