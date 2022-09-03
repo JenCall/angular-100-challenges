71512d3041d628e1a32bc56c34b517114e783cf2
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-03 19:49:32 +0200

    delete some components from module as need adjustments first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { TopOfPageComponent } from './components/top-of-page/top-of-page.component';
 import { RouterModule } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +11,6 @@ import { SharedModule } from './shared/shared.module';
   declarations: [
     AppComponent,
     ToolbarComponent,
-    TopOfPageComponent,
   ],
   imports: [
     BrowserModule,
