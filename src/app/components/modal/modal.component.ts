f811ff6582f6d827158db2ba0116ba3fc456c629
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-05 22:26:47 +0200

    update ts file

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
+  @Input() public title = '';
+  public isHidden = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public close(): void {
+    this.isHidden = true;
   }
 
+  public open(): void {
+    this.isHidden = false;
+  }
 }
