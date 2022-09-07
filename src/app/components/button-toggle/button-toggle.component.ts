9935eb55bdd735e3bc6b31427af849df38b3003b
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-07 22:25:21 +0200

    add buttonMeta class

--- src/app/components/button-toggle/button-toggle.component.ts
+++ src/app/components/button-toggle/button-toggle.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ButtonMeta } from './button-meta.model';
 
 @Component({
   selector: 'app-button-toggle',
   templateUrl: './button-toggle.component.html',
   styleUrls: ['./button-toggle.component.scss']
 })
-export class ButtonToggleComponent implements OnInit {
+export class ButtonToggleComponent {
+  @Input() options: ButtonMeta[] = [];
+  @Output() selection = new EventEmitter<ButtonMeta>();
 
-  constructor() { }
+  public selected(selectedOption: ButtonMeta): void {
+    this.options.forEach((option) => {
+      option.isActive = (selectedOption === option);
+    });
 
-  ngOnInit(): void {
+    this.selection.emit(selectedOption);
   }
-
 }
