8ecdf9cdf60deb9fdf289fc5a29083e97bd2360f
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-14 21:32:23 +0200

    add ts file

--- src/app/components/linkedin-post/linkedin-post.component.ts
+++ src/app/components/linkedin-post/linkedin-post.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-linkedin-post',
   templateUrl: './linkedin-post.component.html',
   styleUrls: ['./linkedin-post.component.scss']
 })
-export class LinkedinPostComponent implements OnInit {
+export class LinkedinPostComponent {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
-  ngOnInit(): void {
+  public get linkedinMessage(): string {
+    const base = 'https://www.linkedin.com/shareArticle?mini=true';
+    const currentSite = `&url=https://www.DylanIsrael.com${this.router.url}`;
+
+    return `${base}${currentSite}`;
   }
 
 }
