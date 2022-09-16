1934eaa5cbeb73b0dcf5e3ddbe0ec3601b169dee
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-16 19:59:53 +0200

    add banner-cut-out ts

--- src/app/components/banner-cut-out/banner-cut-out.component.ts
+++ src/app/components/banner-cut-out/banner-cut-out.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner-cut-out',
   templateUrl: './banner-cut-out.component.html',
   styleUrls: ['./banner-cut-out.component.scss']
 })
-export class BannerCutOutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BannerCutOutComponent  {
+  @Input() public imageUrl = 'assets/back-1.jpg';
+  @Input() public title = '100 Angular Challenge';
 }
