78daab2cc7510a6ab5c29ad5ad951c8942784f13
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-21 20:38:56 +0200

    add skeleton-loader ts

--- src/app/components/skeleton-loader/skeleton-loader.component.ts
+++ src/app/components/skeleton-loader/skeleton-loader.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-skeleton-loader',
   templateUrl: './skeleton-loader.component.html',
   styleUrls: ['./skeleton-loader.component.scss']
 })
-export class SkeletonLoaderComponent implements OnInit {
+export class SkeletonLoaderComponent {
+  @Input() public width = '100%';
+  @Input() public height = '100%';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public get dimensions() {
+    return { width: this.width, height: this.height };
   }
-
 }
