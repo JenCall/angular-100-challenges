02078a34b34493555bae258b85a2ee9ccdbe7731
Jeniffer Callou <jenifferprc@gmail.com>
2022-09-26 07:50:27 +0200

    add tabs ts

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.scss']
+  styleUrls: ['./tabs.component.scss'],
 })
-export class TabsComponent implements OnInit {
+export class TabsComponent {
+  @Input() public tabs: { title: string, active: boolean }[] = [];
+  @Output() public selectedTabIndex = new EventEmitter<number>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public selectTab(selected: { title: string, active: boolean }, index: number): void {
+    this.tabs.forEach((tab) => {
+      tab.active = (selected === tab);
+    });
+    this.selectedTabIndex.emit(index);
   }
-
 }
